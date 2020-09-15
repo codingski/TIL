368178861b398979ca5c55ede0b01a1b26069dcf
Coding_Chan <69722812+codingski@users.noreply.github.com>
2020-09-15 23:41:47 +0900

    Update Nodejs

--- StudyNodejs/web2_nodejs/main.js
+++ StudyNodejs/web2_nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -17,6 +18,7 @@ var app = http.createServer(function(request,response){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href = "/create">create</a>
             ${body}
           </body>
           </html>
@@ -63,6 +65,36 @@ var app = http.createServer(function(request,response){
       });
     }
 
+  }
+  else if(pathname === "/create"){
+    fs.readdir('./data/', function(err, filelist){
+      var title = 'WEB - 1 create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+      <form action="http://localhost:3000/create_process" method = "post">
+        <p><input type = "text" name = "title" placeholder = "title"></p>
+        <p><textarea name = "description" placeholder = "description"></textarea></p>
+        <p><input type = "submit"></p>
+      </form>`);
+      response.writeHead(200);
+      response.end(template);
+      });
+  }
+  else if(pathname === "/create_process"){
+    var body = '';
+    request.on('data', function(data){
+      body += data;
+      console.log(data);
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(){
+        response.writeHead(302, {Location : `/?id=${title}`});
+        response.end(); 
+      })
+    });
   }
   else
   {
